3f255f7c97053943fbe52880ce5f769bdfae12f6
Sungho Park <mrchaos@naver.com>
2022-02-11 15:27:13 +0900

    update

--- src/test/java/org/p2p/solanaj/programs/SystemProgramTest.java
+++ src/test/java/org/p2p/solanaj/programs/SystemProgramTest.java
@@ -24,7 +24,7 @@ public class SystemProgramTest {
 
         assertArrayEquals(new byte[] { 2, 0, 0, 0, -72, 11, 0, 0, 0, 0, 0, 0 }, instruction.getData());
     }
-    @Test
+    @Test    
     public void transferInstructionWithFeePayer() {
         PublicKey fromPublicKey = new PublicKey("QqCCvshxtqMAL2CVALqiJB7uEeE5mjSPsseQdDzsRUo");
         PublicKey toPublickKey = new PublicKey("GrDMoeqMLFjeXQ24H56S1RLgT4R76jsuWCd6SvXyGPQ5");
@@ -35,7 +35,7 @@ public class SystemProgramTest {
             feePayerPublicKey,lamports);
 
         assertEquals(SystemProgram.PROGRAM_ID, instruction.getProgramId());
-        assertEquals(2, instruction.getKeys().size());
+        assertEquals(3, instruction.getKeys().size());
         assertEquals(toPublickKey, instruction.getKeys().get(1).getPublicKey());
 
         assertArrayEquals(new byte[] { 2, 0, 0, 0, -72, 11, 0, 0, 0, 0, 0, 0 }, instruction.getData());
